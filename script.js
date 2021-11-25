f25eb7fe5ded044b5c6dd6a545d9b79c7ed0a41b
alex <bessarabovalex1993@gmail.com>
2021-11-25 22:37:21 +0500

    new

---
+++ script.js
@@ -0,0 +1,70 @@
+var bridge = document.getElementById("bridge"),
+bridgeCanvas = bridge.getContext('2d'),
+brushRadius = (bridge.width / 100) * 5,
+img = new Image();
+if (brushRadius < 50) { brushRadius = 50 }
+
+
+img.onload = function(){  
+    bridgeCanvas.drawImage(img, 0, 0, bridge.width, bridge.height);
+}
+
+img.loc = '';
+/*
+img.filename = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/calgary-bridge-2013.jpg';
+img.filename='rgb('+getRandomInt(0,255)+', '+getRandomInt(0,255)+', '+getRandomInt(0,255)+')';
+*/
+
+img.filename = 'https://i1.wp.com/www.hisour.com/wp-content/uploads/2018/03/Alabama-crimson-color.jpg?w=720&ssl=1';
+if (window.devicePixelRatio >= 2) {
+    var nameParts = img.filename.split('.');
+    img.src = img.loc + nameParts[0]+"-2x"+"."+nameParts[1];
+} else {
+    img.src = img.loc + img.filename;
+} 
+
+
+bridge.addEventListener("mousemove", function(e) {
+    var brushPos = getBrushPos(e.clientX, e.clientY);
+    var leftBut = detectLeftButton(e);
+    // if (leftBut == 1) {
+    //     drawDot(brushPos.x, brushPos.y);
+    // }
+    drawDot(brushPos.x, brushPos.y);
+}, false);
+
+bridge.addEventListener("touchmove", function(e) {
+    e.preventDefault();
+    var touch = e.targetTouches[0];
+    if (touch) {
+        var brushPos = getBrushPos(touch.pageX, touch.pageY);
+        drawDot(brushPos.x, brushPos.y);
+    }
+}, false);
+
+function detectLeftButton(event) {
+    if ('buttons' in event) {
+        return event.buttons === 1;
+    } else if ('which' in event) {
+        return event.which === 1;
+    } else {
+        return event.button === 1;
+    }
+}
+
+
+function getBrushPos(xRef, yRef) {
+    var bridgeRect = bridge.getBoundingClientRect();
+    return {
+      x: Math.floor((xRef - bridgeRect.left) / (bridgeRect.right - bridgeRect.left) * bridge.width),
+      y: Math.floor((yRef - bridgeRect.top) / (bridgeRect.bottom - bridgeRect.top) * bridge.height)
+    };
+}
+
+function drawDot(mouseX,mouseY){
+    bridgeCanvas.beginPath();
+    bridgeCanvas.arc(mouseX, mouseY, brushRadius, 0, 2*Math.PI, true);
+    bridgeCanvas.fillStyle = '#000';
+    bridgeCanvas.globalCompositeOperation = "destination-out";
+    bridgeCanvas.fill();
+}
