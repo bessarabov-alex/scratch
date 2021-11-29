02dc9ca35e77e426e8207170c5d568a637b4dbda
bessarabov-alex <bessarabovalex1993@gmail.com>
2021-11-29 10:03:13 +0500

    api+effect scratch my code for order

--- script.js
+++ script.js
@@ -1,70 +1,52 @@
-var bridge = document.getElementById("bridge"),
-bridgeCanvas = bridge.getContext('2d'),
-brushRadius = (bridge.width / 100) * 5,
-img = new Image();
-if (brushRadius < 50) { brushRadius = 50 }
-
-
-img.onload = function(){  
-    bridgeCanvas.drawImage(img, 0, 0, bridge.width, bridge.height);
-}
-
-img.loc = '';
-/*
-img.filename = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/calgary-bridge-2013.jpg';
-img.filename='rgb('+getRandomInt(0,255)+', '+getRandomInt(0,255)+', '+getRandomInt(0,255)+')';
-*/
-
-img.filename = 'https://i1.wp.com/www.hisour.com/wp-content/uploads/2018/03/Alabama-crimson-color.jpg?w=720&ssl=1';
-if (window.devicePixelRatio >= 2) {
-    var nameParts = img.filename.split('.');
-    img.src = img.loc + nameParts[0]+"-2x"+"."+nameParts[1];
-} else {
-    img.src = img.loc + img.filename;
-} 
-
-
-bridge.addEventListener("mousemove", function(e) {
-    var brushPos = getBrushPos(e.clientX, e.clientY);
-    var leftBut = detectLeftButton(e);
-    // if (leftBut == 1) {
-    //     drawDot(brushPos.x, brushPos.y);
-    // }
-    drawDot(brushPos.x, brushPos.y);
-}, false);
-
-bridge.addEventListener("touchmove", function(e) {
-    e.preventDefault();
-    var touch = e.targetTouches[0];
-    if (touch) {
-        var brushPos = getBrushPos(touch.pageX, touch.pageY);
-        drawDot(brushPos.x, brushPos.y);
+//объявление глобальных переменных
+//mass-хранит значение блок-ссылка
+//i-глобальный счетчик
+let mass=[];
+let url2;
+let i=0;
+//----------------------
+//загрузка изображения в фон карточки главной
+function loadImg(element){
+  //получаем текущий id, с которого вызвали функцию
+  //приводим к одному типу, для булевых операций
+  var ch = Number(element.id);
+  //перебор, пока не найдем блок, с которого был вызов
+  for(i2=0;ch!=(i2-1);i2++){ 
+    if(ch==i2){
+      //получаем ссылку из массива и выводим нужное изображение в блок result
+      url2=mass[i2].name;
+      var img="<img class=new-cat src='"+url2+"' height=300 width=350/>";
+      document.getElementById('result').innerHTML=img;
     }
-}, false);
-
-function detectLeftButton(event) {
-    if ('buttons' in event) {
-        return event.buttons === 1;
-    } else if ('which' in event) {
-        return event.which === 1;
-    } else {
-        return event.button === 1;
-    }
+  }
+  //скрываем блок картинок и выводим детальный блок
+  $(".cards").css("display", "none");
+  $(".detail-page").css("display", "block");
 }
-
-
-function getBrushPos(xRef, yRef) {
-    var bridgeRect = bridge.getBoundingClientRect();
-    return {
-      x: Math.floor((xRef - bridgeRect.left) / (bridgeRect.right - bridgeRect.left) * bridge.width),
-      y: Math.floor((yRef - bridgeRect.top) / (bridgeRect.bottom - bridgeRect.top) * bridge.height)
-    };
+//----------------------
+function mainLoad(){
+    //заполняем все блоки изображениями
+    //после перебора всех id, выходим из цикла
+    firstLoad();
 }
-
-function drawDot(mouseX,mouseY){
-    bridgeCanvas.beginPath();
-    bridgeCanvas.arc(mouseX, mouseY, brushRadius, 0, 2*Math.PI, true);
-    bridgeCanvas.fillStyle = '#000';
-    bridgeCanvas.globalCompositeOperation = "destination-out";
-    bridgeCanvas.fill();
+//----------------------
+//одиночный вывод изображения по id, перебором i
+function firstLoad(){
+    //получаем ссылку из api
+    var link=`https://api.thecatapi.com/v1/images/search`;
+        fetch(link)
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+            console.log(data);
+            var url=data[0].url;
+            var img="<img class=new-cat src='"+url+"' height=300 width=350/>";
+            document.getElementById(i).innerHTML=img;
+          //заполняем массив по ключу - значем ссылки
+          mass[i]={name:url}; 
+          //глобальный счетчик 
+          i++;
+          mainLoad();
+        });
 }
